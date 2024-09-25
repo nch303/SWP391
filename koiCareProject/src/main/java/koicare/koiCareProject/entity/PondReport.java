d0b4f31b234d7fe8468abd9b730ae8aa20b9a493
ThienNgoHuong <thiennhse183982@fpt.edu.vn>
2024-09-25 22:16:33 +0700

    add pondReport

--- koiCareProject/src/main/java/koicare/koiCareProject/entity/PondReport.java
+++ koiCareProject/src/main/java/koicare/koiCareProject/entity/PondReport.java
@@ -1,16 +1,28 @@
-//package koicare.koiCareProject.entity;
-//
-//import jakarta.persistence.Entity;
-//import lombok.Data;
-//
-//import java.util.Date;
-//
-//@Entity
-//@Data
-//public class PondReport {
-//
-//    private int id;
-//    private Date updatedDate;
-//    private
-//
-//}
+package koicare.koiCareProject.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
+public class PondReport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long pondReportID;
+    private Date updatedDate;
+    private double pondTemp;
+    private double pond_pH;
+    private double pondHardness;
+    private double pondAmmonia;
+    private double pondNitrite;
+    private double pondNitrate;
+
+
+
+}
