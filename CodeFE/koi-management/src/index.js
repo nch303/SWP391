9c24ad45371c4b736a0994974eb211f9ed22b27b
AntiHero2311 <hungntse184131@fpt.edu.vn>
2024-09-26 16:57:29 +0700

    update koi info

--- CodeFE/koi-management/src/index.js
+++ CodeFE/koi-management/src/index.js
@@ -5,8 +5,10 @@ import NavBar from './component/header/layout';
 import Home from './page/home/home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NotFound from './page/notfound/Notfound';
-import ManagerKoi from './page/koiFish/manager-koi'; 
+import ManagerKoi from './page/koiFish/manager-koi';
+import Profile from './page/user/profile';
 import ManagerPond from './page/koiPond/manager-pond';
+import KoiInfo from './page/koiFish/koi-info';
 
 function App() {
   return (
@@ -15,7 +17,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/manage-koi" element={<ManagerKoi />} />
-        <Route path="/manage-pond" element={<ManagerPond />}/>
+        <Route path="/manage-pond" element={<ManagerPond />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/koi-info/:id" element={<KoiInfo />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
