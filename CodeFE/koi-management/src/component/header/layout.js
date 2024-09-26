ccf750d1900f79e35ef3f8251acc499881a54e0a
AntiHero2311 <hungntse184131@fpt.edu.vn>
2024-09-26 16:49:12 +0700

    Update layout.js

--- CodeFE/koi-management/src/component/header/layout.js
+++ CodeFE/koi-management/src/component/header/layout.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import './layout.css'; 
-const logoIcon = '/iconlogo.png'; 
+import './layout.css';
+import { Link } from 'react-router-dom';
+const logoIcon = '/iconlogo.png';
 
 const NavBar = () => {
     return (
         <nav className="navbar">
             <div className="logo"><img src={logoIcon} alt="Sunside Koi Care Logo" style={{ width: '25%' }} />Sunside Koi Care</div>
             <ul className="nav-links">
-                <li className="nav-link">Home</li>
+                <li className="nav-link"><a href="/">Home</a></li>
                 <li className="nav-link dropdown">
                     <li className="nav-link">Features</li>
                     <div className="dropdown-content">
-                        <a href="/manage-koi">My Koi</a>
-                        <a href="/manage-pond">My Pond</a>
+                        <Link to="/manage-koi">My Koi</Link>
+                        <Link to="/manage-pond">My Pond</Link>
                         <a href="#">Calculate Food</a>
                         <a href="#">Calculate Salt</a>
                     </div>
                 </li>
                 <li className="nav-link">About us</li>
-                <li className="nav-link">User</li>
+                <li className="nav-link dropdown">
+                    <a href="#">User</a>
+                    <div className="dropdown-content">
+                        <Link to="/user/profile">View Profile</Link>
+                        <a href="#">Buy Plan</a>
+                        <a href="#">Log Out</a>
+                    </div>
+                </li>
             </ul>
         </nav>
     );
