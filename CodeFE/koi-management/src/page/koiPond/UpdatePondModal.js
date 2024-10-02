cbe6ec637a3cd9aee5de1a10a39275426bf148c1
AnPTSE183963 <AnPTSE183963@fpt.edu.vn>
2024-10-02 15:44:32 +0700

    add layout of pond page

--- CodeFE/koi-management/src/page/koiPond/UpdatePondModal.js
+++ CodeFE/koi-management/src/page/koiPond/UpdatePondModal.js
@@ -50,16 +50,37 @@ const UpdatePondModal = ({ pond, onClose, onUpdatePond }) => {
             <div className="modal-content">
                 <h2>Update Pond</h2>
                 <form onSubmit={handleSubmit}>
-                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Pond Name" required />
-                    <input type="number" value={volume} onChange={(e) => setVolume(e.target.value)} placeholder="Volume (l)" required />
-                    <input type="number" value={depth} onChange={(e) => setDepth(e.target.value)} placeholder="Depth (m)" required />
-                    <input type="number" value={pCapacity} onChange={(e) => setPCapacity(e.target.value)} placeholder="Pump Capacity (l/h)" required />
-                    <input type="number" value={drains} onChange={(e) => setDrains(e.target.value)} placeholder="Number of Drains" required />
-                    <input type="number" value={skimmers} onChange={(e) => setSkimmers(e.target.value)} placeholder="Number of Skimmers" required />
-                    <input type="file" onChange={handleImageChange} accept="image/*" />
+                    <div>
+                        <label htmlFor="name">Name: </label>
+                        <input className="input-box" type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Pond Name" required />
+                    </div>
+                    <div>
+                        <label htmlFor="volume">Volume (l): </label>
+                        <input className="input-box" type="number" value={volume} onChange={(e) => setVolume(e.target.value)} placeholder="Volume (l)" required />
+                    </div>
+                    <div>
+                        <label htmlFor="depth">Depth (m): </label>
+                        <input className="input-box" type="number" value={depth} onChange={(e) => setDepth(e.target.value)} placeholder="Depth (m)" required />
+                    </div>
+                    <div>
+                        <label htmlFor="pCapacity">Pump Capacity (l/h): </label>
+                        <input className="input-box" type="number" value={pCapacity} onChange={(e) => setPCapacity(e.target.value)} placeholder="Pump Capacity (l/h)" required />
+                    </div>
+                    <div>
+                        <label htmlFor="drains">Number of Drains: </label>
+                        <input className="input-box" type="number" value={drains} onChange={(e) => setDrains(e.target.value)} placeholder="Number of Drains" required />
+                    </div>
+                    <div>
+                        <label htmlFor="skimmers">Number of Skimmers: </label>
+                        <input className="input-box" type="number" value={skimmers} onChange={(e) => setSkimmers(e.target.value)} placeholder="Number of Skimmers" required />
+                    </div>
+                    <div>
+                        <label htmlFor="image">Choose your pond image</label>
+                        <input className="choose-image" type="file" onChange={handleImageChange} accept="image/*" />
+                    </div>
                     <div className="button-group">
-                        <button type="submit">Update Pond</button>
-                        <button type="button" onClick={onClose}>Cancel</button>
+                        <button className="cancel-button" type="button" onClick={onClose}>Cancel</button>
+                        <button className="confirm-button" type="submit">Update Pond</button>
                     </div>
                 </form>
             </div>
