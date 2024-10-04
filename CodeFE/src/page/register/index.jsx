7e9ba39797c544c781e459c23c19bbcc6fb7b7ff
AntiHero2311 <hungntse184131@fpt.edu.vn>
2024-10-05 00:21:43 +0700

    update resgister

--- CodeFE/src/page/register/index.jsx
+++ CodeFE/src/page/register/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Form, Input, Button, Select } from "antd";
 import api from "../../config/axios";
 import { useNavigate } from "react-router-dom";
+import "./index.scss";
 
 function Register() {
   const navigate = useNavigate();
@@ -21,39 +22,42 @@ function Register() {
   };
 
   return (
-    <Form layout="vertical" onFinish={handleSubmit}>
-      <Form.Item label="Username" name="username">
-        <Input />
-      </Form.Item>
-      <Form.Item label="Password" name="password">
-        <Input />
-      </Form.Item>
-      <Form.Item label="Confirm Password" name="confirmPassword">
-        <Input />
-      </Form.Item>
-      <Form.Item label="Name" name="name">
-        <Input />
-      </Form.Item>
-      <Form.Item label="Phone" name="phone">
-        <Input />
-      </Form.Item>
-      <Form.Item label="Email" name="email">
-        <Input />
-      </Form.Item>
-      <Form.Item label="Role" name="role">
-        <Select>
-          <Select.Option value="MEMBER">MEMBER</Select.Option>
-          <Select.Option value="SHOP">SHOP</Select.Option>
-        </Select>
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Register
-        </Button>
-      </Form.Item>
-
-      
-    </Form>
+    <div className="register">
+      <div className="register-form-container">
+        <Form onFinish={handleSubmit}>
+          <div className="register-title">Register</div>
+          <Form.Item label="Username" name="username">
+            <Input />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input />
+          </Form.Item>
+          <Form.Item label="Confirm Password" name="confirmPassword">
+            <Input />
+          </Form.Item>
+          <Form.Item label="Name" name="name">
+            <Input />
+          </Form.Item>
+          <Form.Item label="Phone" name="phone">
+            <Input />
+          </Form.Item>
+          <Form.Item label="Email" name="email">
+            <Input />
+          </Form.Item>
+          <Form.Item label="Role" name="role">
+            <Select>
+              <Select.Option value="MEMBER">MEMBER</Select.Option>
+              <Select.Option value="SHOP">SHOP</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Register
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
   );
 }
 
