047d539cde79512e06ca9a873dd85d1d0b3b4ab2
nch303 <haoncse184899@fpt.edu.vn>
2024-10-11 13:07:29 +0700

    Revert "update calculateFood ver2"
    This reverts commit 43ca53eddcbae4b3405c6f8ea492968dc2c17a16.

--- CodeBE/koiCareProject/src/main/java/koicare/koiCareProject/APIcontroller/KoiFoodController.java
+++ CodeBE/koiCareProject/src/main/java/koicare/koiCareProject/APIcontroller/KoiFoodController.java
@@ -3,13 +3,10 @@ package koicare.koiCareProject.APIcontroller;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import koicare.koiCareProject.dto.request.KoiFoodRequest;
 import koicare.koiCareProject.dto.response.APIResponse;
-import koicare.koiCareProject.dto.response.KoiFoodListResponse;
 import koicare.koiCareProject.service.KoiFoodService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("api/koifood")
@@ -30,12 +27,4 @@ public class KoiFoodController {
     }
 
     //viết 1 danh sách các con cá trong hồ với lượng thức ăn cho từng con cá
-    @PostMapping("koilist")
-    public  ResponseEntity calculateFoodEachKoiFish(@RequestBody KoiFoodRequest request){
-        APIResponse<List<KoiFoodListResponse>> response = new APIResponse<>();
-
-        response.setResult(koiFoodService.calculateFoodForKoiList(request.getPondID(),request.getTemperature(),request.getLevel()));
-
-        return ResponseEntity.ok(response);
-    }
 }
