5ea45d9e9b420b442d33b55080ae130fc2fac7c6
nch303 <haoncse184899@fpt.edu.vn>
2024-10-11 17:59:56 +0700

    update calculateFood ver 3

---
+++ CodeBE/koiCareProject/src/main/java/koicare/koiCareProject/dto/response/KoiFoodListResponse.java
@@ -0,0 +1,13 @@
+package koicare.koiCareProject.dto.response;
+import lombok.Data;
+import java.util.Date;
+@Data
+public class KoiFoodListResponse {
+    long koiFishID;
+    String koiName;
+    String koiVariety;
+    Date birthday;
+    double length;
+    double weight;
+    double food;
+}
